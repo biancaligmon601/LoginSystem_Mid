b62aa57de2aab0f03c71330c957a15e4a826b71c
biancaligmon601 <biancaligmon93@gmail.com>
2026-04-10 07:27:29 +0800

    Added DBConnection FIXED GAMAY!

--- DBConnection.java
+++ DBConnection.java
@@ -2,19 +2,25 @@ package loginsystem_mid;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DBConnection {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/basic_app";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+
     public static Connection getConnection() {
+
+        Connection conn = null;
+
         try {
-            Connection con = DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/basic_app",
-                "root",
-                ""
-            );
-            return con;
-        } catch (Exception e) {
-            System.out.println(e);
-            return null;
+            conn = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Database Connected Successfully!");
+        } catch (SQLException e) {
+            System.out.println("Connection Failed!");
         }
+
+        return conn;
     }
 }
